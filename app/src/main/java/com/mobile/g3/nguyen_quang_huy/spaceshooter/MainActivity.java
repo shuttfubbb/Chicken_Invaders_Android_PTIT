719598bd5ac92b2752a9133a159ea5259e15a723
Nguyen Quang Huy <shuttfubbb@Nguyens-MacBook-Pro.local>
2023-11-26 14:32:05 +0700

    version 1

---
+++ app/src/main/java/com/mobile/g3/nguyen_quang_huy/spaceshooter/MainActivity.java
@@ -0,0 +1,21 @@
+package com.mobile.g3.nguyen_quang_huy.spaceshooter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set window to fullscreen
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(new Game(this));
+    }
+}
